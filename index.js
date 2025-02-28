d9997c3a9048942ac72c57be1da439fe25891af4
Jamari556 <Jamari.Greene25@compscihigh.org>
2025-02-28 19:23:59 +0000

    Faneto

--- index.js
+++ index.js
@@ -9,20 +9,27 @@ const app = express();
  * STEP 1: Set up a "public" folder and add static files into it
  */
 
+// added css html and the right js file -j
 
 /**
  * STEP 2: Go into dance.html and make sure that it links to both the css and js files
  */
 
+//done -j
+
 /**
  * STEP 3: Add static file middleware. 
  */
 
+app.use(express.static(__dirname + "/public"));
 
 /**
  * STEP 4: Create a route for "/dance" that serves the dance.html file
  */
 
+app.get("/dance", (req, res) => {
+res.sendFile(__dirname + "/public/dance.html")
+})
 
 app.listen(3000, () => {
     console.log(`Server is running`);
